84060ef373b7fd7df407384827f8af4085a24b09
mrohrig <matheus.rohrig@universo.univates.br>
2026-05-29 19:10:06 -0400

    Quinto Commit: reformulação de como o arquivo .csv é salvo

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //importar Scanner para leitura em terminal(Pop-up inventado é bucha)
@@ -80,15 +81,15 @@ public class Main {
                 observacao = null;
 
         String[] funcionarios = null;
-        String auxFuncionarios = "Funcionario(s)";
+        String auxFuncionarios = "";
 
         String[] horas = null;
-        String auxHoras = "Horas";
+        String auxHoras = "";
 
         String[] materiais = null;
         String[] quantidade = null;
-        String auxQuantidades = "Quantidade";
-        String auxMateriais = "Materiais";
+        String auxQuantidades = "";
+        String auxMateriais = "";
 
         //Variavel de validação de seleção
         boolean menuLoop = true;
@@ -116,6 +117,7 @@ public class Main {
             switch(selecao) {
                 //Nome da empresa
                 case 1:
+                    imprimir("Digite o nome da Empresa:\n");
                     empresa = in.nextLine();
                     break;
                 //Funcionario e horas
@@ -123,27 +125,36 @@ public class Main {
                     boolean loop = true;
                     while (loop) {
                         imprimir("Digite o nome do funcionário\n");
-                        auxFuncionarios = auxFuncionarios + ";" + in.nextLine();
+                        imprimir("Para sair digite FIM\n");
+                        String auxLeitura = in.nextLine();
+
+                        if(auxLeitura.toUpperCase().equals("FIM")) {
+                            break;
+                        }
+
+                        auxFuncionarios = auxFuncionarios + ";" + auxLeitura;
                         imprimir("Digite as Horas trabalhadas dele\n");
                         auxHoras = auxHoras + ";" + in.nextLine();
 
-                        imprimir("Deseja realizar a parada do cadastro?\n");
-                        imprimir("1 - Continuar\n");
-                        imprimir("obs:Opção inválida será considerada saída do cadastro\n");
-                        int escolha = in.nextInt();
-                        if(escolha == 1) loop = true;
-                        else loop = false;
                     }
 
                     funcionarios = auxFuncionarios.split(";");
+                    funcionarios = Arrays.stream(funcionarios)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     horas = auxHoras.split(";");
+                    horas = Arrays.stream(horas)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     break;
                 //Responsável técnico
                 case 3:
+                    imprimir("Digite o Responsável técnico\n");
                     respTecnico = in.nextLine();
                     break;
                 //Descrição
                 case 4:
+                    imprimir("Digite a Descrição do serviço\n");
                     descricao = in.nextLine();
                     break;
                 //Materiais utilizados
@@ -151,24 +162,30 @@ public class Main {
                     loop = true;
                     while (loop) {
                         imprimir("Digite o material utilizado\n");
-                        auxMateriais = auxMateriais + ";" + in.nextLine();
+                        imprimir("Para sair digite FIM\n");
+                        String auxLeitura = in.nextLine();
+
+                        if(auxLeitura.toUpperCase().equals("FIM")) {
+                            break;
+                        }
+                        auxMateriais = auxMateriais + ";" + auxLeitura;
                         imprimir("Digite a quantidade utilizada dele\n");
                         auxQuantidades = auxQuantidades + ";" + in.nextLine();
 
-                        imprimir("Deseja realizar a parada do cadastro Materiais\n?");
-                        imprimir("1 - Continuar\n");
-                        imprimir("obs:Opção inválida será considerada saída do cadastro\n");
-                        int escolha = in.nextInt();
-                        if(escolha == 1) loop = true;
-                        else loop = false;
                     }
                     materiais = auxMateriais.split(";");
+                    materiais = Arrays.stream(materiais)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     quantidade = auxQuantidades.split(";");
+                    quantidade = Arrays.stream(quantidade)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     break;
                 //Observação
                 case 6:
+                    imprimir("Digite a observação\n");
                     observacao = in.nextLine();
-                    //menuLoop = false;
                     break;
                 //OS em andamento ou finalizada
                 case 7:
@@ -184,7 +201,6 @@ public class Main {
                         else if (escolha == 2)finalizado = false;
                         else imprimir("Escolha uma opção válida\n");
                     }
-                    //menuLoop = false;
                     break;
                 //Salvar arquivo
                 case 8:
@@ -228,31 +244,49 @@ public class Main {
 
                     //Abre a escrita
                     fileArquivo.abrirEscrita();
-                    //Escreve nome da empresa
-                    fileArquivo.escreverLinha("Nome da Empresa: " + empresa + ";");
-                    //Escreve os serviços realizados
-                    fileArquivo.escreverLinha("Serviços Realizados:; " + descricao + ";");
+                    //Escreve o cabecalho
+                    fileArquivo.escreverLinha("Empresa;Descricao;Responsavel;Funcionario(s);Horas;Observacao;Materiai(s);Quantidade");
+
+                    String dados = empresa + ";" + descricao + ";" + respTecnico + ";";
 
                     //Itera por todos funcionarios e os coloca na lista
-                    if(funcionarios != null && horas != null) {
+                    if(funcionarios != null) {
                         for (int i = 0; i < funcionarios.length; i++) {
-                            fileArquivo.escreverLinha(funcionarios[i] + ";" + horas[i] + ";");
+                            dados = dados + funcionarios[i] + "|";
                         }
                     }
-                    //Escreve o responsavel técnico
-                    fileArquivo.escreverLinha("Responsavel Tecnico:;" + respTecnico + ";");
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+                    //Itera por todos e os coloca na lista
+                    if(horas != null) {
+                        for (int i = 0; i < horas.length; i++) {
+                            dados = dados + horas[i] + "|";
+                        }
+                    }
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+                    //Escreve a observação
+                    dados = dados + observacao + ";";
 
-                    //Escreve a observação se ela existir
-                    if(observacao != null) {
-                        fileArquivo.escreverLinha("Observação:;");
-                        fileArquivo.escreverLinha(observacao + ";");
-                    }
                     //Itera pela lista de materiais e escreve no documento caso existir algum
-                    if(materiais != null && quantidade != null) {
+                    if(materiais != null) {
                         for (int i = 0; i < materiais.length; i++) {
-                            fileArquivo.escreverLinha(materiais[i] + ";" + quantidade[i] + ";");
+                            dados = dados + materiais[i] + "|";
                         }
                     }
+
+                    dados = dados + ";";
+
+                    //Itera pela lista de materiais e escreve no documento caso existir algum
+                    if(quantidade != null) {
+                        for (int i = 0; i < quantidade.length; i++) {
+                            dados = dados + quantidade[i] + "|";
+                        }
+                    }
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+
+                    fileArquivo.escreverLinha(dados);
 
                     //fecha arquivo
                     fileArquivo.fecharArquivo();
@@ -331,37 +365,202 @@ public class Main {
             int selecao = in.nextInt();
             in.nextLine();
 
+            fileEdit.abrirLeitura();
+            //Ler o cabeçalho, atualemnte sem função alguma
+            fileEdit.lerLinha();
+
+            String auxDados = fileEdit.lerLinha();
+        /*
+        posições no array
+        0 - empresa
+        1 - Descricao
+        2 - Responsavel
+        3 - Funcionario(s) - repartir com |
+        4 - Horas
+        5 - Observacao
+        6 - Materiai(s) repartir com |
+        7 - Quantidade
+         */
+            String[] auxArray = auxDados.split(";");
+
+            empresa = auxArray[0];
+            descricao = auxArray[1];
+            respTecnico = auxArray[2];
+            funcionarios = auxArray[3].split("\\|");
+            horas = auxArray[4].split("\\|");
+            observacao = auxArray[5];
+            materiais = auxArray[6].split("\\|");
+            quantidade = auxArray[7].split("\\|");
+
+            fileEdit.fecharArquivo();
+
             switch(selecao){
                 case 1:
-
+                    imprimir("Redigite o nome de empresa:\n");
+                    empresa = in.nextLine();
                     menuLoop = false;
                     break;
                 case 2:
+                    boolean loop = true;
+                    while (loop) {
+                        imprimir("Redigite o nome do funcionário\n");
+                        imprimir("Para sair digite FIM\n");
+                        String auxLeitura = in.nextLine();
 
+                        if(auxLeitura.toUpperCase().equals("FIM")) {
+                            break;
+                        }
+
+                        auxFuncionarios = auxFuncionarios + ";" + auxLeitura;
+                        imprimir("Redigite as Horas trabalhadas dele\n");
+                        auxHoras = auxHoras + ";" + in.nextLine();
+
+                    }
+
+                    funcionarios = auxFuncionarios.split(";");
+                    funcionarios = Arrays.stream(funcionarios)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
+                    horas = auxHoras.split(";");
+                    horas = Arrays.stream(horas)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     menuLoop = false;
                     break;
                 case 3:
-
+                    imprimir("Redigite o novo responśavel  técnico\n");
+                    respTecnico = in.nextLine();
                     menuLoop = false;
                     break;
                 case 4:
-
+                    imprimir("Redigite a descrição de serviço\n");
                     menuLoop = false;
                     break;
                 case 5:
+                    loop = true;
+                    while (loop) {
+                        imprimir("Redigite o(s) material(s) utilizado\n");
+                        imprimir("Para sair digite FIM\n");
+                        String auxLeitura = in.nextLine();
 
+                        if(auxLeitura.toUpperCase().equals("FIM")) {
+                            break;
+                        }
+                        auxMateriais = auxMateriais + ";" + auxLeitura;
+                        imprimir("Redigite a quantidade utilizada dele\n");
+                        auxQuantidades = auxQuantidades + ";" + in.nextLine();
+
+                    }
+                    materiais = auxMateriais.split(";");
+                    materiais = Arrays.stream(materiais)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
+                    quantidade = auxQuantidades.split(";");
+                    quantidade = Arrays.stream(quantidade)
+                            .filter(s -> s != null && !s.trim().isEmpty() && !"null".equalsIgnoreCase(s.trim()))
+                            .toArray(String[]::new);
                     menuLoop = false;
                     break;
                 case 6:
-
+                    imprimir("Redigite a observação\n");
+                    observacao = in.nextLine();
                     menuLoop = false;
                     break;
                 case 7:
+                    int escolha  = 0;
+                    imprimir("A Ordem de Serviço está finalizada?\n");
+                    imprimir("1 - SIM\n");
+                    imprimir("2 - NÂO\n");
+                    while(escolha != 1 && escolha != 2) {
 
+                        escolha = in.nextInt();
+
+                        if(escolha == 1) finalizado = true;
+                        else if (escolha == 2)finalizado = false;
+                        else imprimir("Escolha uma opção válida\n");
+                    }
                     menuLoop = false;
                     break;
                 case 8:
 
+                    LocalDate date = LocalDate.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+                    String actualDate= date.format(formatter);
+
+                    //Primeiro passo, validar os obrigatórios - Sem essa de cadastrar empresa sem nome...
+                    boolean validacao = true;
+                    if(empresa == null) {
+                        System.out.println("O campo empresa precisar ser preenchida");
+                        validacao = false;
+                    }
+                    if(respTecnico == null) {
+                        System.out.println("É necessário ter o nome do responsável");
+                    }
+                    if(funcionarios == null || horas == null) {
+                        System.out.println("É necessário ter o nome e as horas de ao menos um funcionário");
+                    }
+                    if(descricao == null) {
+                        System.out.println("É necessário a descrição do realizado");
+                    }
+                    String filePath = "OS/andamento/";
+
+                    if(finalizado) {
+                        filePath = "OS/finalizada/";
+                    }
+
+
+                    String fileName = "OS/andamento/" + archivesOnFolder[selecaoArchive].getName();
+
+                    Arquivo fileArquivo = new Arquivo(fileName);
+
+                    //Abre a escrita
+                    fileArquivo.abrirEscrita();
+                    //Escreve o cabecalho
+                    fileArquivo.escreverLinha("Empresa;Descricao;Responsavel;Funcionario(s);Horas;Observacao;Materiai(s);Quantidade");
+
+                    String dados = empresa + ";" + descricao + ";" + respTecnico + ";";
+
+                    //Itera por todos funcionarios e os coloca na lista
+                    if(funcionarios != null) {
+                        for (int i = 0; i < funcionarios.length; i++) {
+                            dados = dados + funcionarios[i] + "|";
+                        }
+                    }
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+                    //Itera por todos e os coloca na lista
+                    if(horas != null) {
+                        for (int i = 0; i < horas.length; i++) {
+                            dados = dados + horas[i] + "|";
+                        }
+                    }
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+                    //Escreve a observação
+                    dados = dados + observacao + ";";
+
+                    //Itera pela lista de materiais e escreve no documento caso existir algum
+                    if(materiais != null) {
+                        for (int i = 0; i < materiais.length; i++) {
+                            dados = dados + materiais[i] + "|";
+                        }
+                    }
+
+                    dados = dados + ";";
+
+                    //Itera pela lista de materiais e escreve no documento caso existir algum
+                    if(quantidade != null) {
+                        for (int i = 0; i < quantidade.length; i++) {
+                            dados = dados + quantidade[i] + "|";
+                        }
+                    }
+                    //Caso campo nulo, ao menos delimita
+                    dados = dados + ";";
+
+                    fileArquivo.escreverLinha(dados);
+
+                    //fecha arquivo
+                    fileArquivo.fecharArquivo();
                     menuLoop = false;
                     break;
                 case 9:
@@ -425,20 +624,38 @@ public class Main {
         }
 
         file.abrirLeitura();
+        //Ler o cabeçalho, atualemnte sem função alguma
+        file.lerLinha();
 
-        String auxFimLeitura = null;
-        String auxConcatLeitura = null;
-        do{
-            auxFimLeitura = file.lerLinha();
-            auxConcatLeitura = auxConcatLeitura + auxFimLeitura;
-        }while(auxFimLeitura == null);
+        String auxDados = file.lerLinha();
+        /*
+        posições no array
+        0 - empresa
+        1 - Descricao
+        2 - Responsavel
+        3 - Funcionario(s) - repartir com |
+        4 - Horas
+        5 - Observacao
+        6 - Materiai(s) repartir com |
+        7 - Quantidade
+         */
+        String[] auxArray = auxDados.split(";");
+
+        empresa = auxArray[0];
+        descricao = auxArray[1];
+        respTecnico = auxArray[2];
+        funcionarios = auxArray[3].split("\\|");
+        horas = auxArray[4].split("\\|");
+        observacao = auxArray[5];
+        materiais = auxArray[6].split("\\|");
+        quantidade = auxArray[7].split("\\|");
 
         file.fecharArquivo();
 
         imprimir("========================================\n");
         imprimir("            ORDEM DE SERVIÇO            \n");
         imprimir("========================================\n");
-        imprimir("Estado do serviço:" + auxFinalizado + "\n");
+        imprimir("Estado do serviço: " + auxFinalizado + "\n");
         imprimir("========================================\n");
 
         imprimir("EMPRESA\n");
@@ -450,7 +667,7 @@ public class Main {
         imprimir("----------------------------------------\n");
         for(int i = 0; i < funcionarios.length; i++){
             imprimir("Funcionário " + (i+1) + " :" + funcionarios[i] + "\n");
-            imprimir("Horas trabalhadas"+ horas[i] + "\n");
+            imprimir("Horas trabalhadas: "+ horas[i] + "\n");
         }
         imprimir("\n");
 
